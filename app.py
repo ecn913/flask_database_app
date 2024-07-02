7fbbd02603e8c956b6287b40c684ea200efe8b45
ecn913 <169767313+ecn913@users.noreply.github.com>
2024-07-02 10:44:57 +0100

    Revised error system

--- app.py
+++ app.py
@@ -56,6 +56,15 @@ def delete(todo_id):
     db.session.commit()
     return redirect(url_for("home"))
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
 if __name__=="__main__":
     with app.app_context():
         db.create_all()
